e2ddd19de880a5a7d0f40b7d1483e6e61c8e24f0
max <iosyufeng@sina.com>
2019-01-14 20:57:00 +0800

    fix:更新版本

--- hidde-cell-list/index.js
+++ hidde-cell-list/index.js
@@ -1,9 +1,6 @@
 
 import { NativeModules } from 'react-native';
-
-import testFunc from 'src/testTool'
 import testFunc from "./src/testTool";
-
 const { RNHiddeCellList } = NativeModules;
 
 export default testFunc;
